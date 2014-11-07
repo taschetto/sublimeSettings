33f94c28500c96841b1bf5ce507e418364ea556f
Guilherme Taschetto <gtaschetto@gmail.com>
2014-11-07 11:23:49 -0200

    Add files to be compiled.

--- StarWebTkeDesenv.py
+++ StarWebTkeDesenv.py
@@ -2,15 +2,22 @@ import urllib.parse
 import urllib.request
 import http.cookiejar
 
-url = 'http://stwebdv.thyssenkruppelevadores.com.br/scripts/gisdesenv.pl/swfw3.r'
-values = {'usuario' : 'admin',
-          'senha' : 'tke'}
-
 cj = http.cookiejar.CookieJar()
-data = urllib.parse.urlencode(values).encode('utf-8')
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-req = urllib.request.Request(url, data)
-response = opener.open(req)
-cj.extract_cookies(response, req)
 
-print(cj._cookies)
+loginUrl = 'http://stwebdv.thyssenkruppelevadores.com.br/scripts/gisdesenv.pl/swfw3.r'
+loginData = {'usuario' : 'admin', 'senha' : 'tke'}
+
+postData = urllib.parse.urlencode(loginData).encode('utf-8')
+request = urllib.request.Request(loginUrl, postData)
+response = opener.open(request)
+
+compilerUrl = 'http://stwebdv.thyssenkruppelevadores.com.br/scripts/gisdesenv.pl/progs/swfw0080'
+compilerData = {'Arquivo' : 'c:/sisweb/desenv/ait/ait_aacm1.html,c:/sisweb/desenv/ait/ait_aacm2.html,c:/sisweb/desenv/ait/ait_aacm3.html,'}
+
+postData = urllib.parse.urlencode(compilerData).encode('utf-8')
+request = urllib.request.Request(compilerUrl, postData)
+response = opener.open(request)
+
+page = response.read()
+print(page)
