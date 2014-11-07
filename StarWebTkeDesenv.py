113705cf94a60c8e3f96d27a69eb7ec92270c7fa
Guilherme Taschetto <gtaschetto@gmail.com>
2014-11-07 11:23:01 -0200

    Add StarWeb login support.

--- StarWebTkeDesenv.py
+++ StarWebTkeDesenv.py
@@ -1,4 +1,13 @@
+import urllib.parse
 import urllib.request
-response = urllib.request.urlopen('http://python.org/')
-html = response.read()
-print(html)
+
+url = 'http://stwebdv.thyssenkruppelevadores.com.br/scripts/gisdesenv.pl/swfw3.r'
+values = {'usuario' : 'admin',
+          'senha' : 'tke'}
+
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8') # data should be bytes
+req = urllib.request.Request(url, data)
+response = urllib.request.urlopen(req)
+the_page = response.read()
+print(the_page)
